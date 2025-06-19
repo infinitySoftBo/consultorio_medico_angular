fb5e287b7f777e460ebb9f457d2e12903598bc7b
infinitySoftBo <infinitysoftbolivia@gmail.com>
2025-06-19 05:17:00 -0400

    Fix Angular dependency versions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { AuthService } from './services/auth.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'medico';
-
-  constructor(public auth: AuthService) {}
-
-  logout() {
-    this.auth.logout();
-  }
 }
