598f51c0210911969cf4fc9ee4bd390cc532af5e
infinitySoftBo <infinitysoftbolivia@gmail.com>
2025-06-19 03:17:20 -0400

    Add basic modules and navigation

---
+++ src/app/modules/appointment-calendar/appointment-calendar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-appointment-calendar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './appointment-calendar.component.html',
+  styleUrl: './appointment-calendar.component.css'
+})
+export class AppointmentCalendarComponent {}
---
+++ src/app/modules/payments/payments.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-payments',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './payments.component.html',
+  styleUrl: './payments.component.css'
+})
+export class PaymentsComponent {}
